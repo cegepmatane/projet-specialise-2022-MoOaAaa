0bd6f26b14c78aaa85e4192fa10eeed638932140
Moa <lucas.heidet@orange.fr>
2022-02-07 05:16:19 +0100

    #2 Accéder au mot passé en argument, et afficher sa ligne du lexique. Utilisation d'un nombre de colonnes limité pour plus de practicité

--- code/src/correct.py
+++ code/src/correct.py
@@ -1,5 +1,14 @@
 import pandas as pd
 from pathlib import Path
+import sys
+
+argc = len(sys.argv)
+argv = sys.argv
+mot = argv[1]
+
+if(argc > 2) :
+    print("Ajouter un seul mot")
+    exit(0)
 
 #PoC 1, acceder words Lexique avec python
 cwd = Path.cwd()
@@ -9,9 +18,17 @@ lex = pd.read_csv(filepath,sep='\t')
 print(lex.head(8))
 
 #PoC 2, corriger un mot en s'aidant de lexique
-a = lex[lex["ortho"] == 'a']
+lex1 = lex[["ortho", "phon", "lemme", "cgram"]]
+a = lex1[lex1["ortho"] == 'a']
 print(a)
 print("--------------------------")
-aVerb = lex[(lex["ortho"] == 'a') & (lex["cgram"] == "VER")]
+aVerb = lex1[(lex1["ortho"] == 'a') & (lex1["cgram"] == "VER")]
 print(aVerb)
 print("--------------------------")
+
+print(lex1[lex1["ortho"] == mot])
+
+words = lex["ortho"].to_string(index=False)
+words = words.split("\n")
+words = [word.lstrip() for word in words]
+# print(words)
