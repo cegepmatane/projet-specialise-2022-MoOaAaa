dad04f5535c296e3f0ff1b7d280c12125d1f3de7
Moa <lucas.heidet@orange.fr>
2022-02-07 04:51:45 +0100

    #2 Acceder à un mot précis, et un mot avec un un type de grammaire précis

--- code/src/correct.py
+++ code/src/correct.py
@@ -1,8 +1,17 @@
 import pandas as pd
 from pathlib import Path
 
+#PoC 1, acceder words Lexique avec python
 cwd = Path.cwd()
 Lexique383Path = cwd.parent/"datas"/"Lexique383"/"Lexique383.tsv"
 filepath = Path(Lexique383Path)
 lex = pd.read_csv(filepath,sep='\t')
 print(lex.head(8))
+
+#PoC 2, corriger un mot en s'aidant de lexique
+a = lex[lex["ortho"] == 'a']
+print(a)
+print("--------------------------")
+aVerb = lex[(lex["ortho"] == 'a') & (lex["cgram"] == "VER")]
+print(aVerb)
+print("--------------------------")
