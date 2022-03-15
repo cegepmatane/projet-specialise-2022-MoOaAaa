f7a24a15c6e4a19c7041ce1633bbe938bf591036
Moa <lucas.heidet@orange.fr>
2022-03-15 03:14:59 +0100

    #6 Affichage uniquement des messages des erreurs trouvées et la correction proposée par LanguageTool

--- code/src/GrammarCorrect.py
+++ code/src/GrammarCorrect.py
@@ -2,10 +2,16 @@ import language_tool_python
 
 class GrammarCorrect:
 
-    tool = language_tool_python.LanguageTool('fr')
+
+
+    def __init__(self):
+        self.tool = language_tool_python.LanguageTool('fr')
+
+    def check(self, text):
+        return self.tool.check(text)
 
     def correct(self, text):
-        return self.tool.check(text)
+        return self.tool.correct(text)
 
 text = "Les fleurs mange le pot comme une chien."
 
